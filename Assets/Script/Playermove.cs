af675f076d534833ae8d24c24c556aedb7cf3f23
PNGPhong <phongpngps40540@gmail.com>
2025-01-19 21:12:04 +0700

    player and oject

---
+++ Assets/Script/Playermove.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cameraTransform;
+
+    public float speed = 6f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Animator animator;
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            // Set animation parameter
+            animator.SetFloat("speed", 0.5f); // Hoặc theo tốc độ tính toán của bạn
+        }
+        else
+        {
+            animator.SetFloat("speed", 0f);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            // Set jump animation parameter
+            animator.SetTrigger("Jump");
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
